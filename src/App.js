1c577213f323de38fccc9f9db9fd387dca258d81
jctalavan <jctalavan8@gmail.com>
2022-11-18 15:22:32 +0100

    Change init message on app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          It´s the first webapp that I deploy on Azure Static WebApp.
         </p>
         <a
           className="App-link"
